52d5833cb4da46d187de86a5a532ace6959d651e
Tawfik <tkhorbi@gmail.com>
2025-12-03 22:40:18 +0100

    feat(Fullstack): Updating Register and Login pages

--- frontend/client/src/pages/RegisterPage.tsx
+++ frontend/client/src/pages/RegisterPage.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom';
-import logoImage from '../assets/Logo.svg'; // استيراد اللوغو
+import logoImage from '../assets/Logo.svg'; // استيراد 
 import { AlertCircle, ArrowLeft } from 'lucide-react';
 
 export default function RegisterPage() {
