3b7c3103ccbe0e824ed14c2664175ec643f89772
Tawfik <tkhorbi@gmail.com>
2025-12-08 22:51:33 +0100

    feat(Frontend):  Add Site Setting

--- frontend/client/src/utils/levels.ts
+++ frontend/client/src/utils/levels.ts
@@ -23,6 +23,6 @@ export const getLevelData = (level: number) => {
 };
 
 export const getNextLevelData = (level: number) => {
-  // إذا  للمستوى  نعيد نفس المستوى
+  // إذا    نعيد نفس المستوى
   return LEVEL_SYSTEM.find(l => l.level === level + 1) || null;
 };
