34e7ce915921ae40b5444f51fc0963943af6f443
Tawfik <tkhorbi@gmail.com>
2025-12-18 16:31:23 +0100

    feat(Frontend): Build supervisor reviews app

--- backend/reviews/urls.py
+++ backend/reviews/urls.py
@@ -8,10 +8,8 @@ from .views import (
 )
 
 urlpatterns = [
-    # رابط الرفع
+
     path('submit-recitation/', SubmitRecitationView.as_view(), name='submit-recitation'),
-    
-    # رابط صندوق الوارد للمشرف
     path('pending-reviews/', PendingReviewsView.as_view(), name='pending-reviews'),
     path('submit-feedback/<int:id>/', SubmitFeedbackView.as_view(), name='submit-feedback'),
     path('my-submissions/', StudentSubmissionsView.as_view(), name='my-submissions'),
