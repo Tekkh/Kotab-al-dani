93a6670ff22db77364db4df612e102cc19df10d5
Tawfik <tkhorbi@gmail.com>
2025-12-18 16:30:49 +0100

    feat(Backend): Build supervisor reviews app

--- backend/reviews/urls.py
+++ backend/reviews/urls.py
@@ -16,8 +16,6 @@ urlpatterns = [
     
     # رابط إرسال التصحيح (يحتاج ID التلاوة)
     path('submit-feedback/<int:id>/', SubmitFeedbackView.as_view(), name='submit-feedback'),
-    
-    # رابط الطالب لمشاهدة تلاواته
     path('my-submissions/', StudentSubmissionsView.as_view(), name='my-submissions'),
     path('delete-submission/<int:id>/', DeleteSubmissionView.as_view(), name='delete-submission'),
 ]
